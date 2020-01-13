2ace09502ef0712c0056f15f8fb8403a5086e333
kaynarov96 <student@localhost>
2020-01-13 12:45:06 +0500

    выровнил 47 - 49 строку кода

--- src/js/app.js
+++ src/js/app.js
@@ -6,12 +6,12 @@ const SPECIAL_CATEGORY = 'special';
 
 const purchases = [
     {
-        amount: 15000,
-        category:REGULAR_CATEGORY,
+        amount: 20500,
+        category: REGULAR_CATEGORY,
     },
     {
-        amount: 20000,
-        category:INCREASED_CATEGORY,
+        amount: 20700,
+        category: INCREASED_CATEGORY,
     },
     {
         amount: 9000,
@@ -19,33 +19,32 @@ const purchases = [
     },
     {
         amount: 8600,
-        category:SPECIAL_CATEGORY,
+        category: SPECIAL_CATEGORY,
     },
 ];
 
 const regularPurchasesPercent = 0.01;
 const increasedPurchasesPercent = 0.05;
-const specialPurchasesPercent =0.30;
+const specialPurchasesPercent = 0.30;
 const cashbackLimit = 3000;
 
 let cashbackgive = 0;
 
-function percent(amount, percent){
+function percent(amount, percent) {
     let total = amount * percent / 100;
     return total;
 };
 
-for(const purchase of purchases){
-    if(purchase.category == REGULAR_CATEGORY){
+for (const purchase of purchases) {
+    if (purchase.category == REGULAR_CATEGORY) {
         cashbackgive += percent(purchase.amount, regularPurchasesPercent);
-    } else if(purchase.category == INCREASED_CATEGORY){
+    } else if (purchase.category == INCREASED_CATEGORY) {
         cashbackgive += percent(purchase.amount, increasedPurchasesPercent);
-    } else if(purchase.category == SPECIAL_CATEGORY){
+    } else if (purchase.category == SPECIAL_CATEGORY) {
         cashbackgive += percent(purchase.amount, specialPurchasesPercent);
     }
-    
 }
-if(cashbackgive > cashbackLimit){
-cashbackgive = cashbackLimit};
+if (cashbackgive > cashbackLimit) {
+    cashbackgive = cashbackLimit
+};
 console.log(cashbackgive);
-
